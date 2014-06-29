24531a02ba75489a9b46edab1baf35a969afec7c
Ben Doherty <ben@notionparallax.co.uk>
2014-06-29 21:51:14 +1000

    file to hold the placeholder text

---
+++ letter.js
@@ -0,0 +1,28 @@
+var letter =[
+ "NEIL A. ARMSTRONG"
+,"P.O. BOX 436"
+,"LEBANON, OH 45036"
+,""
+,"July 14, 1994"
+,""
+,"The EMU gang at Johnson Space Center"
+,"Houston, TX 77058"
+,""
+,"To the EMU gang:"
+,"I remember noting a quarter century or so ago that an emu was a 6 foot Australian flightless bird. I thought that got most of it right."
+,"It turned out to be one of the most widely photographed spacecraft in history. That was no doubt due to the fact that it was so photogenic. Equally responsible for its success was its characteristic of hiding from view its ugly occupant."
+,"Its true beauty, however, was that it worked. It was tough, reliable and almost cuddly."
+,"To all of you who made it all that it was, I send a quarter century's worth of thanks and congratulations."
+,""
+,"Sincerely,"
+,""
+,"Neil A. Armstrong"
+].join("\n");
+
+var address = [
+ "Ben Doherty"
+,"3 Meeks Rd"
+,"Marrickville"
+,"2204"
+,"NSW"
+].join("\n");
