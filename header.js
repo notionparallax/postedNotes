0f4afaf5ea685cf2ddd427e0c50fddfee8e89fb8
Ben Doherty <ben@notionparallax.co.uk>
2014-07-06 15:49:30 +1000

    twiddled wording

--- header.js
+++ header.js
@@ -22,12 +22,13 @@ var lolHeader = [
 "     /:/  /       \\::/  /                     \\:\\__\\        \\::/  /  ",
 "     \\/__/         \\/__/                       \\/__/         \\/__/   ",
 "As well as being a useful service, this website is an experiment in how",
-"much can be done without a server. In the constant search for 'leanness' I've",
+"much can be done without a server. In the constant search to be 'lean' I've",
 "always found server set up to slow things down a hell of a lot.",
-"There are emerging ways to get around this (Hoodie etc.) but the humble PayPal",
-"'Pay now' button is the simplest way to validate an idea with real, paying customers!",
+"There are emerging ways to get around this (Hoodie etc.) but the humble ",
+"PayPal 'Pay now' button is the simplest way I've found to validate an idea with",
+"real, paying customers!",
 "I plan to write this up as a tutorial soon, but if you don't have the patience",
-"for that (or oyu just want to say Hi!) then get in touch: ben@notionparallax.co.uk",
+"for that (or you just want to say Hi!) then get in touch: ben@notionparallax.co.uk",
 ];
 
 
